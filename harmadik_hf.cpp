11db1f9f905c8649711ff25e692e882bd72e60ef
Norbert Banyai <norbert.banyai@me.com>
2013-10-11 16:03:55 +0200

    FIX: -Forog a pont (2 módszer) -Kör sugarak beállítása jobb módszerrel -Színek véglegesek -Sok BugFix -Help rendszer -Sugarak kiíratása ha tetszik az épp látott epiciklois -Sugarak beállítása konzolról

--- harmadik_hf.cpp
+++ harmadik_hf.cpp
@@ -4,58 +4,58 @@
 #include <iostream>
 
 GLsizei winWidth = 800, winHeight = 800;
-
 GLint dragged = -1;
+const int N = 60;
 
-GLint Round( GLfloat n ) {return (GLint)( n + 0.5 ); }
-struct Pont {
+
+class Point {
+public:
 	float x,y;
+	void init( float _x, float _y) { this->x = _x; this->y = _y; }
 };
-const int n = 30;
 class Circle
 {
 public:
-	Pont kozepPont;
-	Circle(int x, int y, int r);
-	int r;
-	Pont pontjai [ n +1 ];
-	void rajzol();
-	void szamol();
+	Point midPoint;
+	Circle(int, int, float);
+	float Radius;
+	Point CirclePoints [ N ];
+	void Draw();
+	void CalcCirclePoints();
 
 };
-Circle::Circle(int x, int y, int r)
+Circle::Circle(int x, int y, float Radius)
 {
-	this->r = r;
-	kozepPont.x = x;
-	kozepPont.y = y;
-	float lepes=2*M_PI/n;
+	this->Radius = Radius;
+	midPoint.init(x,y);
+	float step=2*M_PI/N;
 	int m = 0;
-	for(float i=0.0; i<2*M_PI; i+=lepes)
+	for(float i=0.0; i<2*M_PI; i+=step)
 	{
-		pontjai[m].x=kozepPont.x+r*cos(i);
-		pontjai[m].y=kozepPont.y+r*sin(i);
+		CirclePoints[m].x=midPoint.x+Radius*cos(i);
+		CirclePoints[m].y=midPoint.y+Radius*sin(i);
 		m++;
 	}
 }
-void Circle::rajzol()
+void Circle::Draw()
 {
-	for(int i=0; i< n; i++)
+	for(int i=0; i< N; i++)
 	{
-		glVertex2f(this->pontjai[i].x,this->pontjai[i].y);
+		glVertex2f(this->CirclePoints[i].x,this->CirclePoints[i].y);
 	}
 }
-void Circle::szamol()
+void Circle::CalcCirclePoints()
 {
-	float lepes=2*M_PI/n;
+	float step=2*M_PI/N;
 	int m = 0;
-	for(float i=0.0; i<2*M_PI; i+=lepes)
+	for(float i=0.0; i<2*M_PI; i+=step)
 	{
-		pontjai[m].x=kozepPont.x+r*cos(i);
-		pontjai[m].y=kozepPont.y+r*sin(i);
+		CirclePoints[m].x=midPoint.x+Radius*cos(i);
+		CirclePoints[m].y=midPoint.y+Radius*sin(i);
 		m++;
 	}
 }
-
+GLint Round( GLfloat N ) {return (GLint)( N + 0.5 ); }
 void init( ) {
 	glClearColor( 1.0, 1.0, 1.0, 0.0 );
 	glMatrixMode( GL_PROJECTION );
@@ -65,82 +65,82 @@ void init( ) {
 	glPointSize( 10.0 );
 	glLineWidth( 1.2 );
 }
-Circle * nagykor = new Circle(winWidth/2,winHeight/2,100);
-Circle * kiskor = new Circle(600,400,100);
-Pont kozosPont;
-Pont pont;
-Pont pontok[2];
-float meddig = 0;
+Circle * centerCircle = new Circle(winWidth/2,winHeight/2,100);
+Circle * movingCircle = new Circle(600,400,100);
+Point Points[2];
+Point mvCirclePPoint;
+float radLong = 0;
 
 void display( ) {
 	glClear( GL_COLOR_BUFFER_BIT ) ;
-	glColor3f(0.0,0.0,0.0);
 	float x,y;
 
-	glBegin( GL_LINE_LOOP );
-	nagykor->rajzol();
-	glEnd();
-	glBegin( GL_LINE_LOOP );
-	kiskor->rajzol();
+	glColor3f(0.0,0.0,1.0);
+	glBegin( GL_LINE_LOOP );// forgó kör	
+	movingCircle->Draw();
 	glEnd();
 
-	for(int i =0; i< n; i++)
-	{
-		for(int j=0; j<n; j++)
-		{
-			if((kiskor->pontjai[i].x == nagykor->pontjai[j].x) && (kiskor->pontjai[i].y == nagykor->pontjai[j].y))
-			{
-				pontok[0].x = kiskor->pontjai[i].x;
-				pontok[0].y = kiskor->pontjai[i].y;
-			}
-		}
-	}
-
-	glColor3f( 1.0, 0.0, 0.0 );
-	glBegin( GL_POINTS );
-	glVertex2f(pontok[0].x,pontok[0].y);
+	glColor3f(1.0,0.0,0.0);
+	glBegin( GL_LINE_LOOP );// középsõ kör
+	centerCircle->Draw();
 	glEnd();
 
-	glBegin( GL_LINES );
+	glBegin( GL_LINES );	// a csuszka savja
 	glVertex2i(50,50);
 	glVertex2i(750,50);
 	glEnd();
 
-	glBegin( GL_POINTS );
-	glVertex2f(pontok[1].x,pontok[1].y);
+	glBegin( GL_POINTS );	// a csuszka pontja
+	glVertex2f(Points[1].x,Points[1].y);
 	glEnd();
-
-	glColor3f(0.0,0.0,1.0);
-	glBegin( GL_POINTS );
-	glVertex2f(nagykor->kozepPont.x+nagykor->r,nagykor->kozepPont.y);
-	glEnd();
-	// Epiciklois
 
 	glColor3f(0.0,0.0,1.0);
 	glBegin( GL_LINE_STRIP );
-	for (float i=0; i<meddig; i+=0.1)
+	for (float i=0; i<radLong; i+=0.1)
 	{
-		x=(nagykor->r+kiskor->r)*cos(i) - kiskor->r*cos(((nagykor->r+kiskor->r)/(float)kiskor->r)*i);
-		y=(nagykor->r+kiskor->r)*sin(i) - kiskor->r*sin(((nagykor->r+kiskor->r)/(float)kiskor->r)*i);
-		glVertex2f(x+nagykor->kozepPont.x,y+nagykor->kozepPont.y);
-		kiskor->kozepPont.x = (nagykor->r+kiskor->r)*cos(i)+nagykor->kozepPont.x;
-		kiskor->kozepPont.y = (nagykor->r+kiskor->r)*sin(i)+nagykor->kozepPont.y;
-		kiskor->szamol();
+		x=(centerCircle->Radius+movingCircle->Radius)*cos(i) - movingCircle->Radius*cos(((centerCircle->Radius+movingCircle->Radius)/movingCircle->Radius)*i);
+		y=(centerCircle->Radius+movingCircle->Radius)*sin(i) - movingCircle->Radius*sin(((centerCircle->Radius+movingCircle->Radius)/movingCircle->Radius)*i);
+		glVertex2f(x+centerCircle->midPoint.x,y+centerCircle->midPoint.y);
+		movingCircle->midPoint.x = (centerCircle->Radius+movingCircle->Radius)*cos(i)+centerCircle->midPoint.x;
+		movingCircle->midPoint.y = (centerCircle->Radius+movingCircle->Radius)*sin(i)+centerCircle->midPoint.y;
+		movingCircle->CalcCirclePoints();
+		//mvCirclePPoint.x = movingCircle->Radius*cos(2*radLong);
+		//mvCirclePPoint.y = movingCircle->Radius*sin(2*radLong);
+		glutPostRedisplay();
 	}
+	glEnd();
+	/*
+	// Szamoljuk a P pontot...
+	glColor3f(1.0,0.0,0.0);
+	glBegin(GL_POINTS);
+	glVertex2f(movingCircle->midPoint.x-mvCirclePPoint.x,movingCircle->midPoint.y-mvCirclePPoint.y);
+	glEnd();
+	*/
+
+	glBegin(GL_POINTS);	// P pont
+	if(radLong == 0)
+		glVertex2f(centerCircle->midPoint.x+centerCircle->Radius,y+centerCircle->midPoint.y);
+	else
+		glVertex2f(x+centerCircle->midPoint.x,y+centerCircle->midPoint.y);
+	glEnd();
+
+	glColor3f(1.0,0.0,0.0);	// a sugar beallito pont
+	glBegin( GL_POINTS );
+	glVertex2f(Points[0].x,Points[0].y);
 	glEnd();
 
 	glutSwapBuffers( );
 }
 
-GLfloat dist2( Pont P1, Pont P2 ) {
-	GLfloat t1 = P1.x - P2.x;
-	GLfloat t2 = P1.y - P2.y;
-	return t1 * t1 + t2 * t2;
+GLfloat dist2( Point P1, Point P2 ) {
+	GLfloat d1 = P1.x - P2.x;
+	GLfloat d2 = P1.y - P2.y;
+	return d1 * d1 + d2 * d2;
 }
 
-GLint getActivePoint1( Pont *p, GLint size, GLint sens, GLint x, GLint y ) {
+GLint getActivePoint1( Point *p, GLint size, GLint sens, GLint x, GLint y ) {
 	GLint i, s = sens * sens;
-	Pont P;
+	Point P;
 	P.x = x;
 	P.y = y;
 
@@ -150,7 +150,7 @@ GLint getActivePoint1( Pont *p, GLint size, GLint sens, GLint x, GLint y ) {
 	return -1;
 }
 
-GLint getActivePoint2( Pont *p, GLint size, GLint sens, GLint x, GLint y ) {
+GLint getActivePoint2( Point *p, GLint size, GLint sens, GLint x, GLint y ) {
 	GLint i;
 	for ( i = 0; i < size; i++ )
 		if ( fabs( (*( p + i )).x - x ) < sens && fabs( (*( p + i )).y - y ) < sens )
@@ -161,7 +161,7 @@ GLint getActivePoint2( Pont *p, GLint size, GLint sens, GLint x, GLint y ) {
 void processMouse( GLint button, GLint action, GLint xMouse , GLint yMouse ) {
 	GLint i;
 	if ( button == GLUT_LEFT_BUTTON && action == GLUT_DOWN )
-		if ( ( i = getActivePoint1( pontok, 2, 8, xMouse, winHeight - yMouse ) ) != -1 )
+		if ( ( i = getActivePoint1( Points, 2, 8, xMouse, winHeight - yMouse ) ) != -1 )
 			dragged = i;
 	if ( button == GLUT_LEFT_BUTTON && action == GLUT_UP  )
 		dragged = -1;
@@ -171,50 +171,76 @@ void processMouseActiveMotion( GLint xMouse, GLint yMouse ) {
 	GLint i;
 	if ( dragged == 0 )
 	{
-		if( pontok[1].x != 50 )
+		if( Points[1].x != 50 )
 			return;
-		pontok[0].x = xMouse;
-		pontok[0].y = yMouse;
-		nagykor->r = xMouse - nagykor->kozepPont.x;
-		nagykor->szamol();
-		kiskor->r = kiskor->kozepPont.x - xMouse;
-		kiskor->szamol();
+		centerCircle->Radius = xMouse - centerCircle->midPoint.x;
+		centerCircle->CalcCirclePoints();
+		movingCircle->Radius = movingCircle->midPoint.x - xMouse;
+		movingCircle->CalcCirclePoints();
+		Points[0].x = xMouse;
 		glutPostRedisplay( );
 	}
 	else if( dragged > 0 )
 	{
 		if(xMouse>750)
 		{
-			pontok[1].x = 750;
+			Points[1].x = 750;
 			return;
 		}
 		else if(xMouse<50)
 		{
-			pontok[1].x=50;
-			meddig=0;
-			kiskor->kozepPont.x=nagykor->kozepPont.x+nagykor->r+kiskor->r;
-			kiskor->kozepPont.y=nagykor->kozepPont.y;
-			kiskor->szamol();
+			Points[1].x=50;
+			radLong=0;
+			movingCircle->midPoint.x=centerCircle->midPoint.x+centerCircle->Radius+movingCircle->Radius;
+			movingCircle->midPoint.y=centerCircle->midPoint.y;
+			movingCircle->CalcCirclePoints();
+			glutPostRedisplay();
 			return;
 		}
-		pontok[1].x = xMouse;
-		meddig = ((pontok[1].x-50) / 700)*100*M_PI;
+		Points[1].x = xMouse;
+		radLong = ((Points[1].x-50) / 700)*100*M_PI;
 		glutPostRedisplay();
 	}
 }
 void keyboard( unsigned char key, int x, int y ) {
 	switch( key ) {
 	case 'r':
-		meddig = 0;
-		kiskor->kozepPont.x=nagykor->kozepPont.x+nagykor->r+kiskor->r;
-		kiskor->kozepPont.y=nagykor->kozepPont.y;
-		kiskor->szamol();
-		pontok[1].x=50;
+		radLong = 0;
+		movingCircle->midPoint.x=centerCircle->midPoint.x+centerCircle->Radius+movingCircle->Radius;
+		movingCircle->midPoint.y=centerCircle->midPoint.y;
+		movingCircle->CalcCirclePoints();
+		Points[1].x=50;
+		glutPostRedisplay();
 		break;
 	case 't':
-		nagykor->r=kiskor->r=100;
-		nagykor->szamol();
-		kiskor->szamol();
+		centerCircle->Radius=movingCircle->Radius=100;
+		centerCircle->CalcCirclePoints();
+		movingCircle->CalcCirclePoints();
+		Points[0].x = centerCircle->midPoint.x+centerCircle->Radius;
+		glutPostRedisplay();
+		break;
+	case 'd':
+		std::cout << "Kozepso kor sugara: " << centerCircle->Radius << "\nKulso kor sugara: " << movingCircle->Radius << std::endl;
+		break;
+	case 'b':
+		std::cout << "Kozepso kor sugara: ";
+		std::cin >> centerCircle->Radius;
+		std::cout << "Kulso kor sugara: ";
+		std::cin >> movingCircle->Radius;
+		radLong = 0;
+		centerCircle->CalcCirclePoints();
+		movingCircle->midPoint.x=centerCircle->midPoint.x+centerCircle->Radius+movingCircle->Radius;
+		movingCircle->midPoint.y=centerCircle->midPoint.y;
+		movingCircle->CalcCirclePoints();
+		Points[1].x=50;
+		Points[0].x=centerCircle->midPoint.x+centerCircle->Radius;
+		glutPostRedisplay();
+		break;
+	case 'h':
+		std::cout << "r - Visszaallitja a ket kort a kezdo allapotba, de a sugaruk nem valtozik\n";
+		std::cout << "t - Visszaallitja a ket kort a kezdo allpotba, a sugaruk visszaall az EREDETIRE\n";
+		std::cout << "d - Kiirja a ket kor aktualis sugarat\n";
+		std::cout << "b - Bekeri a ket kor sugarat es arra allitja be a koroket (CSAK EGESZ SZAM)\n";
 		break;
 	case 27:
 		exit(1);
@@ -224,13 +250,13 @@ void keyboard( unsigned char key, int x, int y ) {
 }
 
 int main( int argc, char** argv ) {
-	pontok[1].x = 50;
-	pontok[1].y = 50;
+	Points[0].init(centerCircle->midPoint.x+centerCircle->Radius,centerCircle->midPoint.y);
+	Points[1].init(50,50);
 	glutInit( &argc, argv );
 	glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB );
 	glutInitWindowSize( winWidth, winHeight );
 	glutInitWindowPosition( 600,0 );
-	glutCreateWindow( "Harmadik házi" );
+	glutCreateWindow( "HaziFeladat" );
 	init( );
 	glutDisplayFunc( display );
 	glutMouseFunc( processMouse );
